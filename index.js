e72ca68a6befa59c3b3d6012c937204013a9cfd3
1ctinus <jtgc07@hotmail.com>
2021-03-09 21:42:53 -0600

    panic

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const fs = require('fs')
 const path = require('path');
 const pug = require('pug');
 const app = express()
-const port = 3000
-app.use(express.static(path.join(__dirname, 'html')));
+const port = 80
+const hostname = '0.0.0.0'
 app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'html')));
+app.use('/img', express.static(path.join(__dirname, 'img')))
+app.get('/', function (req, res){
+    res.send(pug.renderFile('pug/index.pug'))
+})
 app.get('/*', function (req, res) {
     if (fs.existsSync('pug/' + req.params[0] + '.pug')) {
         res.send("<link href='" + req.params[0] + ".css' rel='stylesheet'>" + pug.renderFile('pug/' + req.params[0] + '.pug'))
@@ -16,7 +21,7 @@ app.get('/*', function (req, res) {
         )
     }
 })
-app.listen(port, () => {
+app.listen(port, hostname, () => {
 
     console.log(`Example app listening at http://localhost:${port}`)
 })
