249886c4b92597e4a830981a26d7e2e72b6d4758
1ctinus <jtgc07@hotmail.com>
2021-03-10 08:39:29 -0600

    file restructering, renamed html to static

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const app = express()
 const port = 80
 const hostname = '0.0.0.0'
 app.use(express.static(path.join(__dirname, 'css')));
-app.use(express.static(path.join(__dirname, 'html')));
+app.use(express.static(path.join(__dirname, 'static')));
 // app.use('/img', express.static(path.join(__dirname, 'img')))
 app.get('/', function (req, res){
     res.send(pug.renderFile('pug/index.pug'))
 })
 app.get('/*', function (req, res) {
     if (fs.existsSync('pug/' + req.params[0] + '.pug')) {
-        res.send("<link href='" + req.params[0] + ".css' rel='stylesheet'>" + pug.renderFile('pug/' + req.params[0] + '.pug'))
+        res.send("<!DOCTYPE html><link href='" + req.params[0] + ".css' rel='stylesheet'>" + pug.renderFile('pug/' + req.params[0] + '.pug'))
     }
     else {
         res.send(
-            fs.readFileSync('html/404.html', 'utf8')
+            fs.readFileSync('static/404.html', 'utf8')
         )
     }
 })
