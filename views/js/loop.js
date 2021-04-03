e84f64ae7670b4870f5c403df8e7a3bc49c8b597
1ctinus <jtgc07@hotmail.com>
2021-04-03 08:28:59 -0500

    iteration now in pug instead

--- views/js/loop.js
+++
@@ -1,6 +0,0 @@
-var arr = "welcome\xa0to\xa0the\xa0bestest\xa0webzone\xa0on\xa0the\xa0internets!".split("")
-var x = "", i
-for (i = 0; i <= 47; i++) {
-  x = x + `<span style="animation-delay:-${i * 100}ms">${arr[i]}</span>`
-}
-document.getElementById("welc").innerHTML = x
