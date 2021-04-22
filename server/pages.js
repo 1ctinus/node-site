2d08cf405764f410ea1785efddd9aa1c33a759b7
1ctinus <jtgc07@hotmail.com>
2021-04-22 16:41:20 -0500

    I hate pushing stuff that i dont know will work, but localhost doesnt seem to work on wine. anyways, less if else and more better express

--- server/pages.js
+++ server/pages.js
@@ -2,9 +2,8 @@ const Router = require("express").Router()               // Express
 const fs = require("fs")                                // File System
 const YAML = require("yaml")                           // Data
 const pug = require("pug")                            // Templating
-const useragent = require("useragent")               // Anti IE Jazz
-const { minify } = require("terser")                // JS Compression
-const CleanCSS = require("clean-css")              // CSS Compression
+
+// const CleanCSS = require("clean-css")              // CSS Compression
 var redirectdata = YAML.parse(fs.readFileSync("data/redirects.yaml", "utf8"))
 
 function title(url){
@@ -30,75 +29,52 @@ Router.get("*", function (req, res) {
     })
   }
   // IE checking
-  let agent = useragent.lookup(req.headers["user-agent"])
-  // No fun for you if you use IE. 
-  if (agent.family == "IE") {
-    res.send("IE is not supported on this site, is outdated, and sucks. Please use Chromium/Firefox based browsers. Thanks.")
-  } else {
-    // Redirect if no slash at the end
-    if (req.url.endsWith("/")) {
-      if (req.url == "/") {
-        defaultRender({
-          "file": "views/pages/index.pug",
-          "style": "/css/index.css",
-          "title": "1ctinus' very cool webzone on the internets"})
-      }
-      else {
-        res.redirect(301, (req.url).substring(0, (req.url).length - 1))
-      }
-    }
-    // checking redirect YAML
-    else if (redirectdata[req.url]) {
-      res.redirect(301, redirectdata[req.url])
-    }
-    else if(req.url == "/shots"|| req.url == "/oc"){
-      let files = fs.readdirSync(`../files${req.url}`).reverse()
-      res.send(200,      defaultRender({"file": `views/templates${req.url}.pug`,
-        "input": files, style: `/css${req.url}.css`}))
-    }
-    else if (fs.existsSync("views" + req.url)) {
-      // Sends compressed Javascript
-      let code = fs.readFileSync(`views${req.url}`, "utf8")
-      if ((req.url).endsWith(".js")) {
-        res.setHeader("content-type", "text/javascript")
-        minify(code)
-          .then(minified => {
-            res.send(minified.code)
-          })
-          .catch(err => console.log(err))
-      }
-
-      // sends compressed CSS, even though i should probably learn SCSS some day.
-      else if ((req.url).endsWith(".css")) {
-        res.setHeader("content-type", "text/css")
-        res.send(new CleanCSS().minify(code).styles)
-      }
-    }
-    else if (fs.existsSync(`views/pages/archive${req.url}.pug`)) {
-      res.redirect(301,  "archive"+req.url)
-    }
-    // Now we get to Pug, main page
-    else if (fs.existsSync(`views/pages${req.url}.pug`)) {
-
-      if (fs.existsSync(`data${req.url}.yaml`)) {
-        const file = fs.readFileSync(`data${req.url}.yaml`, "utf8")
-        var parsedFile = JSON.stringify(YAML.parse(file))
-      } else {
-        // eslint-disable-next-line no-redeclare
-        var parsedFile = ""
-        // messy way of not sending anything if there is no YAML.
-      }
-      defaultRender({
-        "file": `views/pages${req.url}.pug`,
-        "yaml": parsedFile, "style": `/css${req.url}.css`,
-        "title": title(req.url.substring(1))
-      })
-    }
-    else {
-      res.status(404).send(
-        fs.readFileSync("static/404.html", "utf8")
-      )
-    }
+  // let agent = useragent.lookup(req.headers["user-agent"])
+  // // No fun for you if you use IE. 
+  // if (agent.family == "IE") {
+  //   res.send("IE is not supported on this site, is outdated, and sucks. Please use Chromium/Firefox based browsers. Thanks.")
+  // } else {
+  // Redirect if no slash at the end
+  if (req.url == "/") {
+    defaultRender({
+      "file": "views/pages/index.pug",
+      "style": "/css/index.css",
+      "title": "1ctinus' very cool webzone on the internets"})
   }
+  // checking redirect YAML
+  else if (redirectdata[req.url]) {
+    res.redirect(301, redirectdata[req.url])
+  }
+  else if(req.url == "/shots"|| req.url == "/oc"){
+    let files = fs.readdirSync(`../files${req.url}`).reverse()
+    res.send(200,      defaultRender({"file": `views/templates${req.url}.pug`,
+      "input": files, style: `/css${req.url}.css`}))
+  }
+  else if (fs.existsSync(`views/pages/archive${req.url}.pug`)) {
+    res.redirect(301,  "archive"+req.url)
+  }
+  // Now we get to Pug, main page
+  else if (fs.existsSync(`views/pages${req.url}.pug`)) {
+
+    if (fs.existsSync(`data${req.url}.yaml`)) {
+      const file = fs.readFileSync(`data${req.url}.yaml`, "utf8")
+      var parsedFile = JSON.stringify(YAML.parse(file))
+    } else {
+      // eslint-disable-next-line no-redeclare
+      var parsedFile = ""
+      // messy way of not sending anything if there is no YAML.
+    }
+    defaultRender({
+      "file": `views/pages${req.url}.pug`,
+      "yaml": parsedFile, "style": `/css${req.url}.css`,
+      "title": title(req.url.substring(1))
+    })
+  }
+  else {
+    res.status(404).send(
+      fs.readFileSync("static/404.html", "utf8")
+    )
+  }
+  // }
 })
 module.exports = Router
--- server/router.js
+++ server/router.js
@@ -1,8 +1,19 @@
 const Router = require("express").Router()                      // express                               // express                        // pug for... pug
 const { exec } = require("child_process")             // for neofetc
 const rateLimit = require("express-rate-limit")
-
+const CleanCSS = require("clean-css")              // CSS Compression
 const fs = require("fs")                                // file system
+const { minify } = require("terser")                // JS Compression
+const useragent = require("useragent")               // Anti IE Jazz
+const IE = function(req, res, next) {
+  let agent = useragent.lookup(req.headers["user-agent"])
+  // No fun for you if you use IE. 
+  if (agent.family == "IE") {
+    res.send("IE is not supported on this site, is outdated, and sucks. Please use Chromium/Firefox based browsers. Thanks.")
+  } 
+  next()
+}
+Router.use(IE)
 const limiter = rateLimit({
   windowMs: 12 * 60 * 60 * 1000, // 2 minutes
   max: 10, // limit each IP to 100 requests per windowMs
@@ -51,5 +62,19 @@ Router.get("/changelog", function (req, res) {
     }
     res.render("templates/changelog", { git: stdout })
   })
+})
+Router.get("/css/*.css", function (req,res){
+  res.setHeader("content-type", "text/css")
+  res.send(new CleanCSS().minify(fs.readFileSync(`views${req.url}`, "utf8")).styles)
+})
+Router.get("/js/*.js", function(req, res){
+  res.setHeader("content-type", "text/javascript")
+  let code = fs.readFileSync(`views${req.url}`, "utf8")
+  minify(code)
+    .then(({code}) => res.send(code))
+    .catch(console.error)
+})
+Router.get("/.*/", function (req, res){
+  res.redirect(301, (req.url).substring(0, (req.url).length - 1))
 })
 module.exports = Router
