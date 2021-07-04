e23387e6aa0c0dc96a3ed8bf983a16d35a3450eb
1ctinus <jtgc07@hotmail.com>
2021-07-04 10:32:55 -0500

    CSS added, now we're talking

--- views/js/pie.js
+++ views/js/pie.js
@@ -17,28 +17,32 @@ function getStart(len) {
     //  console.log(val)
     return (-1*(Math.PI/50 * (val * 100/sum))+ 3*Math.PI/2)
 }
-ctx.clearRect(0, 0, c.width, c.height);
+ctx.fillStyle = "#121014"
+ctx.fillRect(0, 0, c.width, c.height);
 ctx.font = "60px sans-serif";
 ctx.textAlign = "center"; 
+ctx.fillStyle = "indigo"
+ctx.fillText(document.querySelector("#title").value, c.width/2 +3,53);
+ctx.fillStyle = "aliceblue"
 ctx.fillText(document.querySelector("#title").value, c.width/2,50); 
 for (let j = 0; j < document.querySelector("tbody").children.length; j++){
-    ctx.strokeStyle = "#FFF"
+    ctx.strokeStyle = "#dce"
     ctx.lineWidth = 3;
     ctx.beginPath();
-ctx.arc(400, 400, 200, getStart(j), (j === 0) ? 3 * Math.PI / 2 : getStart(j-1));
-ctx.lineTo(400, 400)
+ctx.arc(400, 300, 200, getStart(j), (j === 0) ? 3 * Math.PI / 2 : getStart(j-1));
+ctx.lineTo(400, 300)
 ctx.closePath();
 ctx.fillStyle = document.querySelector("tbody").children[j].children[2].children[0].value
 ctx.fill();
 ctx.stroke();
 ctx.lineWidth = 2;
-ctx.strokeStyle = "#000"
+ctx.strokeStyle = "aliceblue"
 ctx.beginPath();
 ctx.rect(625, 200 + j*25, 20, 20);
 ctx.fill();
 ctx.stroke();
 ctx.font = "20px sans-serif";
-ctx.fillStyle = "black"
+ctx.fillStyle = "aliceblue"
 ctx.textAlign = "left"; 
 ctx.fillText(document.querySelector("tbody").children[j].children[0].children[0].value, 650, 217 + j*25); 
 }
@@ -53,3 +57,13 @@ document.querySelector("#add").addEventListener("click",e => {
     document.querySelector("tbody").appendChild(newTR);
     init()
 })
+document.getElementById("save").addEventListener("click",
+  function () {
+    let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', 'pie.png');
+    let dataURL = c.toDataURL('image/png');
+    let url = dataURL
+    downloadLink.setAttribute('href', url);
+    downloadLink.click();
+  }
+)
